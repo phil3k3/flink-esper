ef827b50a5fa9a928001cb6f6bce08950b191330
phil3k <philip.limbeck@gmail.com>
2017-10-25 20:44:22 +0200

    moved Esper Engine to value state, started with type serialization

--- src/main/java/at/datasciencelabs/EsperStream.java
+++ src/main/java/at/datasciencelabs/EsperStream.java
@@ -1,11 +1,18 @@
 package at.datasciencelabs;
 
+import org.apache.flink.api.common.functions.InvalidTypesException;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.functions.NullByteKeySelector;
+import org.apache.flink.api.java.typeutils.ResultTypeQueryable;
+import org.apache.flink.api.java.typeutils.TypeExtractionException;
+import org.apache.flink.api.java.typeutils.TypeExtractionUtils;
+import org.apache.flink.api.java.typeutils.TypeExtractor;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+
+import java.lang.reflect.Type;
 
 
 /**
@@ -32,17 +39,36 @@ public class EsperStream<IN> {
      * Select from the EsperStream, must provide the return type of the output DataStream since no type information is
      * currently extracted from the @see {@link EsperSelectFunction}.
      */
-    public <R> SingleOutputStreamOperator<R> select(EsperSelectFunction<R> esperSelectFunction, TypeInformation<R> dataStreamReturnType) {
+    public <R> SingleOutputStreamOperator<R> select(EsperSelectFunction<R> esperSelectFunction) {
         KeySelector<IN, Byte> keySelector = new NullByteKeySelector<>();
 
         SingleOutputStreamOperator<R> patternStream;
 
-        // TODO until the type extractor is capable of extracting non-generic parameters, the return type has to be passed in manually
+        TypeInformation<R> typeInformation = getTypeInformation(esperSelectFunction);
 
         final boolean isProcessingTime = inputStream.getExecutionEnvironment().getStreamTimeCharacteristic() == TimeCharacteristic.ProcessingTime;
-        patternStream = inputStream.keyBy(keySelector).transform("SelectEsperOperator", dataStreamReturnType, new SelectEsperStreamOperator<Byte, IN, R>(inputStream.getType(), esperSelectFunction, isProcessingTime, esperQuery));
+        patternStream = inputStream.keyBy(keySelector).transform("SelectEsperOperator", typeInformation, new SelectEsperStreamOperator<Byte, IN, R>(inputStream.getType(), esperSelectFunction, isProcessingTime, esperQuery));
 
         return patternStream;
+    }
+
+    @SuppressWarnings("unchecked")
+    private <OUT> TypeInformation<OUT> getTypeInformation(EsperSelectFunction<OUT> esperSelectFunction) {
+        try {
+            TypeExtractionUtils.LambdaExecutable lambdaExecutable = TypeExtractionUtils.checkAndExtractLambda(esperSelectFunction);
+            if (esperSelectFunction instanceof ResultTypeQueryable) {
+                return ((ResultTypeQueryable<OUT>) esperSelectFunction).getProducedType();
+            }
+            if (lambdaExecutable != null) {
+                Type type = lambdaExecutable.getReturnType();
+                return (TypeInformation<OUT>) TypeExtractor.createTypeInfo(type);
+            }
+            else {
+                return TypeExtractor.createTypeInfo(esperSelectFunction, EsperSelectFunction.class, esperSelectFunction.getClass(), 0);
+            }
+        } catch (TypeExtractionException e) {
+            throw new InvalidTypesException("Could not extract types.", e);
+        }
     }
 
 }
--- src/test/java/at/datasciencelabs/EsperStreamTest.java
+++ src/test/java/at/datasciencelabs/EsperStreamTest.java
@@ -1,18 +1,72 @@
 package at.datasciencelabs;
 
-import org.apache.flink.api.common.typeinfo.TypeInformation;
+import com.espertech.esper.client.EventBean;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.streaming.util.StreamingMultipleProgramsTestBase;
+import org.junit.Before;
 import org.junit.Test;
 
-public class EsperStreamTest {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNull.notNullValue;
+
+public class EsperStreamTest extends StreamingMultipleProgramsTestBase implements Serializable {
+
+    private static List<TestEvent> result;
+
+
+    @Before
+    public void before() {
+        result = new ArrayList<>();
+    }
 
     @Test
-    public void testEsperStream() throws Exception {
-
+    @SuppressWarnings("Convert2Lambda")
+    public void shouldSelectFromStreamUsingAnonymousClassSelect() throws Exception {
         StreamExecutionEnvironment executionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
+        executionEnvironment.setParallelism(1);
 
         DataStream<TestEvent> dataStream = executionEnvironment.fromElements(new TestEvent("peter", 10), new TestEvent("alex", 25), new TestEvent("maria", 30));
+
+        EsperStream<TestEvent> esperStream = new EsperStream<>(dataStream, "select name, age from TestEvent");
+
+        DataStream<TestEvent> resultStream = esperStream.select(new EsperSelectFunction<TestEvent>() {
+            @Override
+            public TestEvent select(EventBean eventBean) throws Exception {
+                String name = (String) eventBean.get("name");
+                int age = (int) eventBean.get("age");
+                return new TestEvent(name, age);
+            }
+        });
+
+        resultStream.addSink(new SinkFunction<TestEvent>() {
+            @Override
+            public void invoke(TestEvent testEvent) throws Exception {
+                System.err.println(testEvent);
+                result.add(testEvent);
+            }
+        });
+
+        executionEnvironment.execute("test-2");
+
+        assertThat(result, is(notNullValue()));
+        assertThat(result.size(), is(3));
+    }
+
+    @Test
+    @SuppressWarnings("Convert2Lambda")
+    public void shouldSelectFromStreamUsingLambdaSelect() throws Exception {
+
+        StreamExecutionEnvironment executionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
+        executionEnvironment.setParallelism(1);
+
+        DataStream<TestEvent> dataStream = executionEnvironment.fromElements(new TestEvent("peter1", 10), new TestEvent("alex1", 25), new TestEvent("maria1", 30));
 
         EsperStream<TestEvent> esperStream = new EsperStream<>(dataStream, "select name, age from TestEvent");
 
@@ -20,48 +74,19 @@ public class EsperStreamTest {
             String name = (String) collector.get("name");
             int age = (int) collector.get("age");
             return new TestEvent(name, age);
-        }, TypeInformation.of(TestEvent.class));
+        });
 
-        resultStream.printToErr();
+        resultStream.addSink(new SinkFunction<TestEvent>() {
+            @Override
+            public void invoke(TestEvent testEvent) throws Exception {
+                result.add(testEvent);
+            }
+        });
 
-        executionEnvironment.execute("test");
+        executionEnvironment.execute("test-1");
 
-    }
-
-    @Test
-    public void testMoreComplexEsperStream() throws Exception {
-        StreamExecutionEnvironment executionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
-
-        DataStream<TestEvent> dataStream = executionEnvironment.fromElements(new TestEvent("peter", 10), new TestEvent("alex", 25), new TestEvent("maria", 30));
-
-        EsperStream<TestEvent> eventEsperStream = new EsperStream<>(dataStream, "select avg(age) as average_age from TestEvent");
-
-        DataStream<Double> resultStream = eventEsperStream.select((EsperSelectFunction<Double>) collector -> {
-            Double age = (Double) collector.get("average_age");
-            return age;
-        }, TypeInformation.of(Double.class));
-
-        resultStream.printToErr();
-
-        executionEnvironment.execute("test");
-    }
-
-    @Test
-    public void testStateChangeRule() throws Exception {
-        StreamExecutionEnvironment executionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
-
-        DataStream<TestEvent> dataStream = executionEnvironment.fromElements(new TestEvent("peter", 10), new TestEvent("alex", 25));
-
-        EsperStream<TestEvent> eventEsperStream = new EsperStream<>(dataStream, "select avg(age) as average_age from TestEvent");
-
-        DataStream<Double> resultStream = eventEsperStream.select((EsperSelectFunction<Double>) collector -> {
-            Double age = (Double) collector.get("average_age");
-            return age;
-        }, TypeInformation.of(Double.class));
-
-        resultStream.printToErr();
-
-        executionEnvironment.execute("stateChange1");
+        assertThat(result, is(notNullValue()));
+        assertThat(result.size(), is(3));
     }
 
 }
