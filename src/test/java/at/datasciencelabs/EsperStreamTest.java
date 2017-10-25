d26a7c2366c4fe8a47ec9d98140fe8a8fc82b31b
phil3k <philip.limbeck@gmail.com>
2017-10-25 20:44:22 +0200

    type extraction, started with unit tests WIP (DO NOT PUSH LIKE THIS!)

--- src/test/java/at/datasciencelabs/EsperStreamTest.java
+++ src/test/java/at/datasciencelabs/EsperStreamTest.java
@@ -1,14 +1,24 @@
 package at.datasciencelabs;
 
-import org.apache.flink.api.common.typeinfo.TypeInformation;
+import com.espertech.esper.client.EventBean;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.junit.Assert;
 import org.junit.Test;
 
-public class EsperStreamTest {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNull.notNullValue;
+
+public class EsperStreamTest implements Serializable {
 
     @Test
-    public void testEsperStream() throws Exception {
+    public void shouldSelectFromStreamUsingLambdaSelect() throws Exception {
 
         StreamExecutionEnvironment executionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
 
@@ -20,48 +30,57 @@ public class EsperStreamTest {
             String name = (String) collector.get("name");
             int age = (int) collector.get("age");
             return new TestEvent(name, age);
-        }, TypeInformation.of(TestEvent.class));
+        });
+
+        final List<TestEvent> result = new ArrayList<>();
+        resultStream.addSink((SinkFunction<TestEvent>) result::add);
+
+        executionEnvironment.execute("test");
+
+        assertThat(result, is(notNullValue()));
+        assertThat(result.size(), is(3));
+    }
+
+    @Test
+    @SuppressWarnings("Convert2Lambda")
+    public void shouldSelectFromStreamUsingAnonymousClassSelect() throws Exception {
+        StreamExecutionEnvironment executionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<TestEvent> dataStream = executionEnvironment.fromElements(new TestEvent("peter", 10), new TestEvent("alex", 25), new TestEvent("maria", 30));
+
+        EsperStream<TestEvent> esperStream = new EsperStream<>(dataStream, "select name, age from TestEvent");
+
+        DataStream<TestEvent> resultStream = esperStream.select(new EsperSelectFunction<TestEvent>() {
+            @Override
+            public TestEvent select(EventBean eventBean) throws Exception {
+                String name = (String) eventBean.get("name");
+                int age = (int) eventBean.get("age");
+                return new TestEvent(name, age);
+            }
+        });
 
         resultStream.printToErr();
 
         executionEnvironment.execute("test");
 
+        Assert.fail("not finished");
     }
 
     @Test
-    public void testMoreComplexEsperStream() throws Exception {
+    public void shouldCalculateAverageAge() throws Exception {
         StreamExecutionEnvironment executionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
 
         DataStream<TestEvent> dataStream = executionEnvironment.fromElements(new TestEvent("peter", 10), new TestEvent("alex", 25), new TestEvent("maria", 30));
 
         EsperStream<TestEvent> eventEsperStream = new EsperStream<>(dataStream, "select avg(age) as average_age from TestEvent");
 
-        DataStream<Double> resultStream = eventEsperStream.select((EsperSelectFunction<Double>) collector -> {
-            Double age = (Double) collector.get("average_age");
-            return age;
-        }, TypeInformation.of(Double.class));
+        DataStream<Double> resultStream = eventEsperStream.select((EsperSelectFunction<Double>) collector -> (Double) collector.get("average_age"));
 
         resultStream.printToErr();
 
         executionEnvironment.execute("test");
-    }
 
-    @Test
-    public void testStateChangeRule() throws Exception {
-        StreamExecutionEnvironment executionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
-
-        DataStream<TestEvent> dataStream = executionEnvironment.fromElements(new TestEvent("peter", 10), new TestEvent("alex", 25));
-
-        EsperStream<TestEvent> eventEsperStream = new EsperStream<>(dataStream, "select avg(age) as average_age from TestEvent");
-
-        DataStream<Double> resultStream = eventEsperStream.select((EsperSelectFunction<Double>) collector -> {
-            Double age = (Double) collector.get("average_age");
-            return age;
-        }, TypeInformation.of(Double.class));
-
-        resultStream.printToErr();
-
-        executionEnvironment.execute("stateChange1");
+        Assert.fail("not finished");
     }
 
 }
